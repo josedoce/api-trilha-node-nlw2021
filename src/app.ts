aba003c05225f88448de2b820ccf5d34d9003c9c
josedoce <josesamya@gmail.com>
2021-02-26 17:42:33 -0300

    atualização para o express tratar erros

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import createConnection from './database';
+import { AppError } from './errors/AppError';
 import { router } from './routes';
 
 createConnection();
@@ -14,5 +16,17 @@ const app = express();
  */
 app.use(express.json());
 app.use(router);
+//configurando o app para capturar os erros usando o midleware
+app.use((err: Error, req: Request, res: Response, _next: NextFunction)=>{
+    if(err instanceof AppError){
+        return res.status(err.statusCode).json({
+            message: err.message
+        })        
+    }
+    return res.status(500).json({
+        status: "Error",
+        message: `Internal server error ${err.message}`
+    })
+})
 
 export { app };
