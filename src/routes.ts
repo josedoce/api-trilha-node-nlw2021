d5631dea9452c134339b665ae067b5726b3cd11f
josedoce <josesamya@gmail.com>
2021-02-25 23:12:00 -0300

    rotas

--- src/routes.ts
+++ src/routes.ts
@@ -4,11 +4,15 @@ const router = Router();
 //controllers
 import {UserController} from './controllers/UserController';
 import {SurveysController} from './controllers/SurveyController';
+import { SendMailController } from './controllers/SendMailController';
 
 const userController = new UserController();
 const surveyController = new SurveysController();
+const sendMailController = new SendMailController();
+
 router.post('/users', userController.create);
 router.post('/surveys', surveyController.create);
 router.get('/surveys', surveyController.show);
+router.post('/send_mail', sendMailController.execute);
 
 export {router};
